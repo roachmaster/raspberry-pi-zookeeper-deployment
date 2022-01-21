92f09851e6586598a031cec4433569dbdcdc299b
Leonardo <leonardo.rocha1990@gmail.com>
2022-01-20 21:05:26 -0600

    updating docker file set up

--- docker/setup-start-zookeeper.sh
+++ docker/setup-start-zookeeper.sh
@@ -6,6 +6,7 @@ echo $ZK_DATA_DIR
 for (( i = 0; i < $REPLICA_SET_NUM; i++ ))
 do
   j=$((i+1)) ;
+  echo "server.${j}=zk-${i}.zk-hs:2888:3888";
   echo "server.${j}=zk-${i}.zk-hs:2888:3888" >> /opt/zookeeper/conf/zoo.cfg
 done
 
@@ -13,6 +14,8 @@ done
 ZK_SERVER_ID=$((${HOSTNAME##*-}+1))
 echo "${ZK_SERVER_ID}" > "${ZK_DATA_DIR}/myid"
 
+cat "${ZK_DATA_DIR}/myid"
+cat /opt/zookeeper/conf/zoo.cfg
 # run server
 zkServer.sh start-foreground /opt/zookeeper/conf/zoo.cfg
 
